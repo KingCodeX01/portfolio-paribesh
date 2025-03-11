db5cba813cfd184b9298a8aa44608bca1a27cab3
KingCodeX01 <shresthaparibesh7@gmail.com>
2025-03-11 16:37:30 +0545

    Paribesh Portfolio begin

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Navbar from "./components/navbar";
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
